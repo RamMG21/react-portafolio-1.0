118da983ff20d3ac9027740a439483ac56ede397
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2023-06-24 22:54:38 -0700

    Background and sections
    Header, body, footer

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Ramses Moreno</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/reference_letters/Reference_letters.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Reference_letters.css'
+
+const Reference_letters = () => {
+  return (
+    <div>Reference_letters</div>
+  )
+}
+
+export default Reference_letters
